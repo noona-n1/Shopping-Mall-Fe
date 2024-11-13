ba78251ec03433d7a76e9009b6996f722afd237a
Doni <zwol10@naver.com>
2024-11-14 01:16:32 +0900

    feat : 로그인 창 반응형 480 768 1100

--- src/page/LoginPage/LoginPage.jsx
+++ src/page/LoginPage/LoginPage.jsx
@@ -6,30 +6,32 @@ const LoginPage = () => {
   return (
     <>
       <div className='sub-title-wrab'>
-        <p className='sub-title'>LOGIN</p>
-      </div>
-      <div className='member-join'>
-        <div className='join-content'>
-          <div className='form-container'>
-            <div className='login-wrab'>
-              <p>
-                <label>이메일 아이디</label>
-                <input type='text' className='form-control' placeholder='이메일 입력' />
-              </p>
-              <p>
-                <label>비밀번호</label>
-                <input type='password' className='form-control' placeholder='비밀번호 입력' />
-              </p>
-              <p>
-                <label class='custom-checkbox'>
-                  <input type='checkbox' />
-                  <span class='checkmark' />
-                  이메일 저장
-                </label>
-              </p>
+        <div className='sub-title-name'>
+          <p className='sub-title'>LOGIN</p>
+        </div>
+        <div className='member-join'>
+          <div className='join-content'>
+            <div className='form-container'>
+              <div className='login-wrab'>
+                <p className='input-line'>
+                  <label>이메일 아이디</label>
+                  <input type='text' className='form-control' placeholder='이메일 입력' />
+                </p>
+                <p className='input-line'>
+                  <label>비밀번호</label>
+                  <input type='password' className='form-control' placeholder='비밀번호 입력' />
+                </p>
+              </div>
+              <div>
+                <button className='login-button'>로그인</button>
+              </div>
             </div>
-            <div>
-              <button className='login-button'>로그인</button>
+            <div className='login-option'>
+              <label class='custom-checkbox'>
+                <input type='checkbox' />
+                <span class='checkmark' />
+                이메일 저장
+              </label>
               <ul className='link'>
                 <li>
                   <a href='#' className='right-line'>
@@ -43,16 +45,16 @@ const LoginPage = () => {
             </div>
           </div>
         </div>
-      </div>
-
-      <div className='join-email'>
-        <div className='email-content'>이메일로 간편하고 빠르게!</div>
-        <button className='email-signup'>이메일로 가입하기</button>
-      </div>
-
-      <div className='join-sns'>
-        <div className='sns-content'>SNS계정으로 of you를 이용해보세요</div>
-        <button className='sns-sighup'>카카오로 시작하기</button>
+        <div className='join'>
+          <div className='join-email'>
+            <div className='email-content'>이메일로 간편하고 빠르게!</div>
+            <button className='email-signup'>이메일로 가입하기</button>
+          </div>
+          <div className='join-sns'>
+            <div className='sns-content'>SNS계정으로 of you를 이용해보세요</div>
+            <button className='sns-signup'>카카오로 시작하기</button>
+          </div>
+        </div>
       </div>
     </>
   );
