d931d0d8f84c2f1e7dfb8839fd1332bc3754313e
Doni <zwol10@naver.com>
2024-11-13 10:32:50 +0900

    test : API 연결 시도

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import HomePage from './../page/HomePage/HomePage';
+import LoginPage from '../page/LoginPage/LoginPage';
 
 function AppRouter() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
     </Routes>
   );
 }
