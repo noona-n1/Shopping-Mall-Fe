57edd87ad0f78aeeb34913eaebee87bb48075cf3
Doni <zwol10@naver.com>
2024-11-13 20:45:45 +0900

    feat : login join창 구현

--- src/page/LoginPage/LoginPage.jsx
+++ src/page/LoginPage/LoginPage.jsx
@@ -20,8 +20,27 @@ const LoginPage = () => {
                 <label>비밀번호</label>
                 <input type='password' className='form-control' placeholder='비밀번호 입력' />
               </p>
+              <p>
+                <label class='custom-checkbox'>
+                  <input type='checkbox' />
+                  <span class='checkmark' />
+                  이메일 저장
+                </label>
+              </p>
             </div>
-            <button className='login-button'>로그인</button>
+            <div>
+              <button className='login-button'>로그인</button>
+              <ul className='link'>
+                <li>
+                  <a href='#' className='right-line'>
+                    아이디 찾기
+                  </a>
+                </li>
+                <li>
+                  <a href='#'>비밀번호 찾기</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
